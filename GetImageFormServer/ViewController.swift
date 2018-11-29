77f42c9c9ac34cb508155ef9efbc59b7910b55e5
LIYANA <liyana@liyanas-mac.local>
2018-11-29 15:06:13 +0530

    Initial Commit

---
+++ GetImageFormServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetImageFormServer
+//
+//  Created by LIYANA on 29/11/18.
+//  Copyright © 2018 LIYANA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
